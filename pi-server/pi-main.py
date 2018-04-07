009cbe174b981c284b20313c2f20c831f4e41c0c
BuildTools <meade.jonathan@gmail.com>
2018-04-07 11:48:18 -0600

    pi server updates

--- pi-server/pi-main.py
+++ pi-server/pi-main.py
@@ -10,24 +10,26 @@ def build_iptable_rule(packet_data):
     rule_dict = {}
     rule_dict['-A'] = 'OUTPUT'
     rule_dict['-p'] = 'udp'
+    rule_dict['-j'] = 'DROP'
     rule_dict['-d'] = packet_data['dest_ip']
     rule_string = "iptables "
     for key in rule_dict.keys:
         rule_string += " " + str(key) + " " + str(rule_dict[key])
     return
 
-@app.route('/check', methods=['POST'])
+@app.route('/response', methods=['POST'])
 def receive_response():
     """
     Takes in a POST response from the soc server with a set of ips and a yes/no status.
     """
     if request.method == 'POST':
-        packet_data = {}
-        packet_data['src_ip'] = request.form['src_ip']
-        packet_data['dest_ip'] = request.form['dest_ip']
-        packet_data['status'] = request.form['status']        
-        if packet_data['status'] == 'bad':
-            #TODO: Agree on what status flags should be
-            new_rule = build_iptable_rule(packet_data)
+        ip_data = request.form[rules]
+        rule_data['dest_ip'] = ip_data['ip']
+        rule_data['status'] = ip_data['status']        
+        if ip_data['status'] == 'RED':
+            new_rule = build_iptable_rule(rule_data)
             execute_iptable_rule(new_rule)
+	elif ip_data['status'] == 'AMBER':
+            #TODO: Notify user
+            return
     return
