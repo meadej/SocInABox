1f90d203e72e212dfcb5be34c121d28fba2e7bd1
BuildTools <meade.jonathan@gmail.com>
2018-04-07 14:59:00 -0600

    web updates

--- soc_box/soc_box.py
+++ soc_box/soc_box.py
@@ -1,11 +1,27 @@
 import requests as req
-import os
-import json
+import psutil
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
+def get_connections():
+    conns=psutil.net_connections()
+    return_arr = []
+    for conn in conns:
+        if len(conn[4]) != 0 and conn[4][0] not in return_arr:
+            return_arr.append(conn[4][0])
+    return return_arr
+
 @app.route("/")
 def ui_connect():
-    return render_template('dashboard.html', devices=[{'name':'Phillips Lightbulb', 'ip':'192.168.1.1'}])   
+    connected_devices = []
+    for addr in get_connections():
+       connected_devices.append({
+           'name':'Unknown',
+           'ip':addr
+       })
+    return render_template('dashboard.html', devices=connected_devices)   
+
+if __name__ == "__main__":
+    get_connections()
