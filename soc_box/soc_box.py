a0380e1b4b009f750910c47136fd4eada7d52717
Spencer Hanson <spencer.hanson@swimlane.com>
2018-04-07 23:59:03 -0600

    Code cleanup

--- soc_box/soc_box.py
+++ soc_box/soc_box.py
@@ -5,13 +5,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 def get_connections():
-    conns=psutil.net_connections()
+    conns = psutil.net_connections()
     return_arr = []
     for conn in conns:
         if len(conn[4]) != 0 and conn[4][0] not in return_arr:
             return_arr.append(conn[4][0])
     return return_arr
+
 
 @app.route("/")
 def ui_connect():
@@ -25,11 +27,11 @@ def ui_connect():
             location = 'Local'
             ipType = 'N/a'
         else:
-            resp = req.get('http://extreme-ip-lookup.com/json/'+str(addr)).json()
+            resp = req.get('http://extreme-ip-lookup.com/json/' + str(addr)).json()
             ipType = resp['ipType']
             if resp['country'] != '':
                 if resp['city'] != '':
-                    location = resp['city']+', '+resp['country']
+                    location = resp['city'] + ', ' + resp['country']
                 else:
                     location = resp['country']
             # unknown location
@@ -39,14 +41,14 @@ def ui_connect():
             if ipType == '':
                 ipType = 'Unknown'
 
-
         connected_devices.append({
-            'name':'Unknown',
-            'ip':addr,
-            'location':location,
-            'ipType':ipType
+            'name': 'Unknown',
+            'ip': addr,
+            'location': location,
+            'ipType': ipType
         })
-    return render_template('dashboard.html', devices=connected_devices)   
+    return render_template('dashboard.html', devices=connected_devices)
+
 
 if __name__ == "__main__":
     get_connections()
