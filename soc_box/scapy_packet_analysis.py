ad2d94dc41b695f63c7c992a17ae736e8214d74f
nzimm <nizi2734@colorado.edu>
2018-04-08 10:30:59 -0600

    Added pcap support

--- soc_box/scapy_packet_analysis.py
+++ soc_box/scapy_packet_analysis.py
@@ -52,21 +52,31 @@ def main():
     parser.add_argument('count', help='number of packets to sniff (because ctrl-C wont work', type=int)
     parser.add_argument('--server', help='send header info to server', action='store_true')
     parser.add_argument('-v', '--verbose', help='print verbose output', action='store_true')
+    parser.add_argument('pcap', help='pcap file to send', type=str, nargs='?')
     args=parser.parse_args()
 
     i = 0
-    while i < args.count:
+    if args.pcap:
+        with open(args.pcap) as FILE:
+            packets = rdpcap(FILE)
+            for pkt in packets:
+                handle_pkt(pkt)
+                i += 1
+                if I < args.count:
+                    break
+    else:
+        while i < args.count:
 
-        pkt = sniff(count=1, store=1)
-        header_info = handle_pkt(pkt[0])
-        if header_info == None:
-            continue
-        else:
-            if args.server:
-                print(requests.post("http://127.0.0.1:5000/check", json=header_info).json())
-            if args.verbose:
-                print(header_info)
-            i += 1
+            pkt = sniff(count=1, store=1)
+            header_info = handle_pkt(pkt[0])
+            if header_info == None:
+                continue
+            else:
+                if args.server:
+                    print(requests.post("http://127.0.0.1:5000/check", json=header_info).json())
+                if args.verbose:
+                    print(header_info)
+                i += 1
 
 if __name__ == '__main__':
     main()
